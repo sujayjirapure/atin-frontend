00dfb9b866a88c056c48678735b496c5b2ca49ab
sujay <jirpuresujay@gmail.com>
2025-12-28 16:45:00 +0530

    wp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
-import { Features } from "./components/features";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
+import WhatsAppFloat from "./components/WhatsAppFloat";
+
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
@@ -26,6 +25,7 @@ const App = () => {
   return (
     <div>
       <Navigation />
+      <WhatsAppFloat />
       <Header data={landingPageData.Header} />
       {/* <Features data={landingPageData.Features} /> */}
       <About data={landingPageData.About} />
@@ -35,6 +35,7 @@ const App = () => {
       
       <Team data={landingPageData.Team} /> */}
       <Contact data={landingPageData.Contact} />
+      
     </div>
   );
 };
---
+++ src/components/WhatsAppFloat.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../App.css";
+
+export default function WhatsAppFloat() {
+  const phone = "919146034000"; // replace with your WhatsApp number
+  const message = encodeURIComponent(
+   "Hi Akola Telecom & IP Network Pvt Ltd Team,\nPlease contact me."
+  );
+
+  const openWhatsApp = () => {
+    window.open(
+      `https://wa.me/${phone}?text=${message}`,
+      "_blank"
+    );
+  };
+
+  return (
+    <div className="whatsapp-float" onClick={openWhatsApp}>
+      💬
+    </div>
+  );
+}
